371e53e60adcb681a36e687cb3331ed07d15eb87
Thomas Mayfield <tmayfield@axiomsoftwareinc.com>
2007-12-19 16:56:01 +0000

    [project @ 2636] Add domain / draftHost validation.
    **i AXCMS-646

--- ContentManagementSystem/macros.js
+++ ContentManagementSystem/macros.js
@@ -6,6 +6,50 @@ function delete_objects(){
 	app.getObjects([], req.data.objs.map(function(obj){ return "_id: "+obj.id }).join(" OR ")).invoke('cms_delete');
 }
 
+function domain_warning(){
+	var host = req.data.http_host;
+	var staging_hosts = app.getDomains(1);
+	var preview_hosts = app.getDomains(2);
+	var errors = [];
+
+	// if-cms-version-enterprise
+	switch(staging_hosts.length){
+	case 0: 	errors.push("No staging domains set."); break;
+	case 1:		errors.push("Only one domain set for staging layer."); break;
+	} 
+	
+	if(preview_hosts.length === 0){
+		errors.push("No preview domains set.");
+	}
+	
+	var on_staging = false;
+	for each(var staging_host in staging_hosts){
+		if(staging_host == host) {
+			on_staging = true;
+			break;
+		}
+	}
+
+	if(!on_staging){
+		errors.push("Accessing CMS on live domain. Axiom CMS Enterprise must be accessed via a staging domain.");
+	}
+
+	// end-cms-if
+	// if-cms-version-professional|standard
+	if(staging_hosts.length === 0){
+		errors.push("No preview domain");
+	} else if(staging_hosts.length > 1) {
+		errors.push("Multiple preview domains set.");
+	}
+	// end-cms-if
+
+	if(errors.length === 0){
+		return false;
+	} else {
+		return new XMLList(['<span class="error_message" style="display:inline">WARNING: </span>'+x for each(x in errors)].join('<br/>'));
+	}
+
+}
 
 function delete_tags(){
 	var tag_folder = root.get('tagfolder');
