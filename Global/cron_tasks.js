5abf8ae3a454d331c36e5d002646cf26468b94c4
Thomas Mayfield <tmayfield@axiomsoftwareinc.com>
2008-05-02 17:27:49 +0000

    [project @ 3629] Add cmsCleanupAdvice hook. **i AXCMS-258

--- Global/cron_tasks.js
+++ Global/cron_tasks.js
@@ -3,25 +3,29 @@ function cms_cleanup(){
 	var ms_to_hours = 1000*60*60;
 	var now = (new Date()).getTime();
 	var cleanup = cmsGlobals.props.cleanup;
-	
+
 	var expiration_time;
 	if(cleanup && cleanup.@interval){
 		expiration_time = parseInt(cleanup.@interval, 10);
 		if(!expiration_time){
 			app.log("Could not parse interval for cms cleanup - defaulting to 72 hours.");
 			expiration_time = 72;
-		} 
+		}
 	} else {
 		//default to three days
-		expiration_time =  72; 
+		expiration_time =  72;
 	}
 
-	var i = 0;
-	for each(obj in app.getObjects([], "_status: null")){
-		if( expiration_time < Math.floor((now - obj.lastmodified().getTime()) / ms_to_hours)) {
-			obj._parent.remove(obj);
-			i++;
-		}
-	}         
-	app.log('Removed '+i+' objects with null status.');
+	var for_removal = [obj for each(obj in app.getObjects([], "_status: null")) if(expiration_time < Math.floor((now - obj.lastmodified().getTime()) / ms_to_hours)) ];
+
+	// advice hook
+	if(typeof ContentManagementSystem.cmsCleanupAdvice == 'function'){
+		ContentManagementSystem.cmsCleanupAdvice(for_removal);
+	}
+
+	for each(obj in for_removal){
+		obj._parent.remove(obj);
+	}
+
+	app.log('Removed '+for_removal.length+' objects with null status.');
 }
