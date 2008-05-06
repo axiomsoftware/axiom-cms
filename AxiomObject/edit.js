e483d0d0bced4686db1c9930b1c52aeaa21f7833
Thomas Mayfield <tmayfield@axiomsoftwareinc.com>
2008-05-06 17:20:16 +0000

    [project @ 3663] Fix not being able to clear Date fields. **i AXCMS-571

--- AxiomObject/edit.js
+++ AxiomObject/edit.js
@@ -1,19 +1,25 @@
 function preprocess_data(data){
-		if(data.ax_id){
-			// workaround for IE bug with name||id = "id" busting things
-			data.id = data.ax_id;
-			delete data.ax_id;
+	var schema = this.getSchema();
+	for(var i in data){
+		if(data[i] === '' && schema[i].type != 'string'){
+			data[i] = null;
 		}
-		if(data['_location'] === ''){
-			var folder = app.getHits("CMSContentFolder", {id: this._prototype}).objects(0,1)[0];
-			if(!folder){
-				folder = root.get('cms');
-			}
-			data['_location'] = folder.getPath();
-		} 
-		if(this._action != "Added"){
-			data['_action'] = 'Edited';
+	}
+	if(data.ax_id){
+		// workaround for IE bug with name||id = "id" busting things
+		data.id = data.ax_id;
+		delete data.ax_id;
+	}
+	if(data['_location'] === ''){
+		var folder = app.getHits("CMSContentFolder", {id: this._prototype}).objects(0,1)[0];
+		if(!folder){
+			folder = root.get('cms');
 		}
+		data['_location'] = folder.getPath();
+	}
+	if(this._action != "Added"){
+		data['_action'] = 'Edited';
+	}
 
 	return data;
 }
@@ -49,14 +55,14 @@ function save(data){
 
 			var conn = app.getDbSource("_default").getConnection(false);
 			var audit_data = {username:  session.user.username,
-				object_id: this._id,
-				task_id:   this._task ? this._task.getTarget().task_id : null,
-				uri:       this.getURI(),
-				title:     this.title,
-				prototype: this._prototype,
-				action:    action};
-			
-			if(this._task && (action == "Added" || !previous_task || (previous_task.getTarget().task_id != this._task.getTarget().task_id))){		
+							  object_id: this._id,
+							  task_id:   this._task ? this._task.getTarget().task_id : null,
+							  uri:       this.getURI(),
+							  title:     this.title,
+							  prototype: this._prototype,
+							  action:    action};
+
+			if(this._task && (action == "Added" || !previous_task || (previous_task.getTarget().task_id != this._task.getTarget().task_id))){
 				audit_data.added_to_task = 'TRUE';
 			}
 			auditLogObjectAction(audit_data, conn);
@@ -65,12 +71,12 @@ function save(data){
 		}
 		return errors;
 	// if-cms-version-enterprise
- 	}
+ 	};
 	return save_obj.call(app.getDraft(this, 1), data);
 	// end-cms-if
 }
 
 function save_as_preview(data){
 	data = this.preprocess_data(data);
-	return this.edit(data)
+	return this.edit(data);
 }
