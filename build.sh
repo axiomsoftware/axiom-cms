f1fc5a2c95ec8b5e33acd98b037e89396db94034
Thomas Mayfield <tmayfield@axiomsoftwareinc.com>
2008-07-29 15:43:54 -0400

    move build script to bzr

--- build.sh
+++ build.sh
@@ -1,20 +1,13 @@
-/usr/local/bin/ruby cms-build.rb
+#!/bin/bash
 
-mkdir svn-cms-enterprise
-mkdir svn-cms-standard
+ruby cms-build.rb
 
-svn co svn+ssh://tmayfield@file.axiomstack.com/opt/svn/svnrepos/dev/build/cms/3.1/enterprise/ svn-cms-enterprise
-svn co svn+ssh://tmayfield@file.axiomstack.com/opt/svn/svnrepos/dev/build/cms/3.1/standard/ svn-cms-standard
+rsync --exclude=".bzr" --delete -avr cms-enterprise/* ../cms-enterprise-build
+rsync --exclude=".bzr" --delete -avr cms-standard/* ../cms-standard-build
 
-svn co svn+ssh://tmayfield@file.axiomstack.com/opt/svn/svnrepos/dev/src/stack/3.1/svn-sync.rb
-
-rsync --exclude=".svn" --delete -avr cms-enterprise/* svn-cms-enterprise
-rsync --exclude=".svn" --delete -avr cms-standard/* svn-cms-standard
-
-find ./svn-cms-enterprise -name ".svn" -prune -o -exec svn add {} \;
-find ./svn-cms-standard -name ".svn" -prune -o -exec svn add {} \;
-
-svn commit svn-cms-enterprise -m  "Automatic Bamboo Commit **i AXSTK-253"
-svn commit svn-cms-standard -m "Automatic Bamboo Commit **i AXSTK-253"
-
-rm -rf svn-cms-*
+cd ../cms-enterprise-build
+bzr add
+bzr commit -m "enterprise autobuild"
+cd ../cms-standard-build
+bzr add
+bzr commit -m "standard autobuild"
