4089ed82da01d42c56fc24a5edd70c642d3d87f8
Nicholas Campbell <ncampbell@axiomsoftwareinc.com>
2008-11-17 19:55:24 -0500

    Fixes for CMSTaskContainer being loaded in the standard version. Also includes a change that allows modules to load their own cms.xml files.

--- Global/onstart.js
+++ Global/onstart.js
@@ -11,16 +11,16 @@ function cms_init(){
 		cms.id = 'cms';
 		cms.title = 'CMS';
 		root.add(cms);
-	
+
 		var tag_folder = new CMSTagFolder();
 		tag_folder.id = 'tagfolder';
 		cms.add(tag_folder);
-		
+
 		var users = new CMSUserFolder();
 		users.id = "userfolder";
 		users.title = "Users";
 		cms.add(users);
-		
+
 		var files = new CMSFileFolder();
 		files.id = "filefolder";
 		files.title = "File Folder";
@@ -45,23 +45,25 @@ function cms_init(){
 		bin.id = "recyclebin";
 	}
 
+	// if-cms-version-enterprise
 	if(!app.getObjects("CMSTaskContainer", "_d:1", {maxlength: 1})[0]){
  		var container = new CMSTaskContainer();
 		root.get('cms').add(container);
 		container.id = "task_container";
 	}
+	// end-cms-if
 
 	// setup audit log table
 	var exception = false;
 	var conn = app.getDbSource('_default').getConnection(false);
 	try{
 		// object action table
-		
+
 		var stmt_obj_action = conn.createStatement();
 		stmt_obj_action.execute('CREATE TABLE AuditLog_ObjectActions( '+
 								'timestamp TIMESTAMP DEFAULT CURRENT_TIMESTAMP,'+
 								'object_id INT NOT NULL,'+
-								'task_id INT,'+ 
+								'task_id INT,'+
 								'prototype VARCHAR(255),'+
 								'added_to_task BOOLEAN,'+
 								'title VARCHAR(255),'+
@@ -116,38 +118,60 @@ function cms_init(){
 	// end-cms-if
 }
 
+function getCMSXMLLines(file) {
+    var lines = [];
+    if (file.exists()) {
+	var reader = null;
+	try {
+	    reader = new java.io.BufferedReader(new java.io.FileReader(file));
+	    var line = "";
+	    while ((line = reader.readLine()) != null) {
+		if (line != "<prototypes>" && line != "</prototypes>") {
+		    lines.push(line.trim());
+		}
+	    }
+	    reader.close();
+	} catch(e) {
+	    app.log("Error loading "+file.getName());
+	    app.log(e);
+	} finally{
+	    if (reader != null) {
+		try {
+		    reader.close();
+		} catch (e) {
+		    app.log(e);
+		}
+	    }
+	}
+    }
+
+    return lines;
+}
+
 cmsGlobals.loadCMSProperties = function() {
     var reader = null;
-	try {
-	    // create cms properties
-	    var cmsPropertiesXML = "<prototypes></prototypes>";
-	    var cmsFile = new java.io.File(app.getDir() + java.io.File.separator + "cms.xml");
-	    if (cmsFile.exists()) {
-	    	cmsPropertiesXML = "";
-	    	reader = new java.io.BufferedReader(new java.io.FileReader(cmsFile));
-	    	var line = "";
-			var lines = [];
-	    	while ((line = reader.readLine()) != null) {
-	    		lines.push(line);
-	    	}
-			cmsPropertiesXML = lines.join("");
-	    	reader.close();	
-	    } else {
-	    	logEvent("Warning: cms.xml not found."); 
-	    }
-    	cmsGlobals.props = new XML(cmsPropertiesXML);
-	} catch(e) {
-		app.log("Error loading cms.xml");
-		app.log(e);
-	} finally{
-		if (reader != null) {
-			try { 
-				reader.close(); 
-			} catch (e) { 
-				app.log(e); 
-			}
-		}
-	}
+    // Application must define a cms.xml for the cms to function properly
+    var cmsPropertiesXML = new XML("<prototypes></prototypes>");
+    var cms_lines = [];
+    try {
+	var cmsFile = new java.io.File(app.getDir() + java.io.File.separator + "cms.xml");
+	cms_lines = getCMSXMLLines(cmsFile);
+    } catch (e) {
+	app.log("Error: File 'cms.xml' was not present in the application. It is required to run the Axiom CMS.");
+    }
+
+    // Find cms.xml for loaded modules (in order of specification) (optional)
+    var modules = app.getProperty("modules").split(",");
+    for each (var mod in modules) {
+	var modcmsFile = new java.io.File("modules/"+mod+"/cms.xml");
+	cms_lines = cms_lines.concat(getCMSXMLLines(modcmsFile));
+    }
+
+    if (cms_lines.length > 0) {
+	cmsPropertiesXML.prototypes += new XMLList(cms_lines.join(""));
+    }
+
+    cmsGlobals.props = cmsPropertiesXML;
 }
 
 /*
@@ -175,7 +199,7 @@ function getCMSPrototypes() {
     public static final int CMS_PREVIEW_HEIGHT = 250;
 
     public void addCMSThumbnails() throws Exception {
-        ImageObject thumb = this.jsFunction_bound(ImageObject.CMS_THUMB_WIDTH, 
+        ImageObject thumb = this.jsFunction_bound(ImageObject.CMS_THUMB_WIDTH,
                                                   ImageObject.CMS_THUMB_HEIGHT, false);
         if (thumb != null) {
             this.jsFunction_addThumbnail(thumb, "thumb");
@@ -183,7 +207,7 @@ function getCMSPrototypes() {
             core.app.logError("Could not create 'thumb' for " + this.node.getString(FILE_NAME));
         }
 
-        ImageObject preview = this.jsFunction_bound(ImageObject.CMS_PREVIEW_WIDTH, 
+        ImageObject preview = this.jsFunction_bound(ImageObject.CMS_PREVIEW_WIDTH,
                                                     ImageObject.CMS_PREVIEW_HEIGHT, false);
         if (preview != null) {
             this.jsFunction_addThumbnail(preview, "preview");
