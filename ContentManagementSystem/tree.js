d12c67175a5ba1d11ee332462f01efb95a106d4c
Nicholas Campbell <ncampbell@axiomsoftwareinc.com>
2008-12-01 16:22:51 -0500

    Tree fix and onstart fix. Some old code made it's way into the onstart class.

--- ContentManagementSystem/tree.js
+++ ContentManagementSystem/tree.js
@@ -4,15 +4,15 @@ function tree() {
 	var prototypes = this.getSearchablePrototypes();
 	var cms_prototypes = ['ContentManagementSystem','CMSContentFolder'];
 	var tree_prototypes = Array.union(prototypes,cms_prototypes);
-
-	var children = node.getChildren(tree_prototypes,{},{sort:{cms_lastmodified:'desc'}}).map(function(obj){
+    var cms_filter = new OrFilter({cms_status:"z"},{cms_status:"a"});
+    var children = node.getChildren(tree_prototypes, cms_filter,{sort:{cms_lastmodified:'desc'}}).map(function(obj){
 		return {
 			_id: obj._id,
 			title: obj.title ? obj.title: 'Untitled Object',
 			icon_uri: obj.getTreeIconURI(),
 			location: obj.getURI(),
 			prototype: obj.getPrettyName().toString() ? obj.getPrettyName().toString() : obj._prototype,
-			hasChildren: obj.hasChildren(),
+		    hasChildren: (obj.getChildCount([],cms_filter)>0),
 			created: obj.cms_createdby,
 			last_modified: obj.cms_lasteditedby,
 			refs: obj.ref_list().*.toXMLString(),
