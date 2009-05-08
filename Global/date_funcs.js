f7b948dc2030809a2244106fda84600a3c947a91
Andy Ray <andy@siteworx.com>
2009-05-08 09:30:22 -0400

    Fixing month showing one behind

--- Global/date_funcs.js
+++ Global/date_funcs.js
@@ -22,10 +22,10 @@
 
 
 Date.prototype.toCSVString = function(){
-	return this.pad(this.getMonth())+'/'+this.pad(this.getDate())+'/'+this.getFullYear().toString().substring(2,4) + ' '+this.pad(this.getHours())+':'+this.pad(this.getMinutes());
+	return this.pad(this.getMonth()+1)+'/'+this.pad(this.getDate())+'/'+this.getFullYear().toString().substring(2,4) + ' '+this.pad(this.getHours())+':'+this.pad(this.getMinutes());
 } 
 Date.prototype.toCMSString = function(){
-	return this.pad(this.getMonth())+'/'+this.pad(this.getDate())+'/'+this.getFullYear().toString().substring(2,4) + ', '+this.pad(this.getHours())+':'+this.pad(this.getMinutes());
+	return this.pad(this.getMonth()+1)+'/'+this.pad(this.getDate())+'/'+this.getFullYear().toString().substring(2,4) + ', '+this.pad(this.getHours())+':'+this.pad(this.getMinutes());
 } 
 Date.prototype.pad = function(i){
 	return i < 10 ? '0'+i : i;
