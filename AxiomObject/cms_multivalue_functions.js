0c55fb82860436b63a1ed761bef9e096acd6bcf2
Will Wolf <willwolf@hq-172.16.11.24.phones.siteworx.local>
2009-07-09 17:36:30 -0400

    fixed bug where we try to access .getTarget() on an object that doesnt exist

--- AxiomObject/cms_multivalue_functions.js
+++ AxiomObject/cms_multivalue_functions.js
@@ -33,11 +33,13 @@ function multiValueJSON(property) {
         var last = this[prop].length-1;
         var o = null;
         for (var i = 0;i<this[prop].length;i++) {
-            o = this[prop][i].getTarget();
-            if (o) {
-                json += '["'+escape(o.title)+'","'+o.getPath()+'","'+o.getURI()+'"]';
-                if (i!=last) { json += ','; }
-            }
+			if(this[prop][i]){
+				o = this[prop][i].getTarget();
+				if (o) {
+					json += '["'+escape(o.title)+'","'+o.getPath()+'","'+o.getURI()+'"]';
+					if (i!=last) { json += ','; }
+				}
+			}
         }
     }
     if (requested) {
